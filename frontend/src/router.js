58114a8479fe0b64323a09aa5b65ae52cb409b26
ILABUTK <ilab.utk@gmail.com>
2017-09-18 10:09:14 -0400

    Add front end template.

---
+++ frontend/src/router.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+function load (component) {
+  // '@' is aliased to src/components
+  return () => import(`@/${component}.vue`)
+}
+
+export default new VueRouter({
+  /*
+   * NOTE! VueRouter "history" mode DOESN'T works for Cordova builds,
+   * it is only to be used only for websites.
+   *
+   * If you decide to go with "history" mode, please also open /config/index.js
+   * and set "build.publicPath" to something other than an empty string.
+   * Example: '/' instead of current ''
+   *
+   * If switching back to default "hash" mode, don't forget to set the
+   * build publicPath back to '' so Cordova builds work again.
+   */
+
+  routes: [
+    { path: '/',
+      component: load('Main'),
+      children: [
+        {path: 'showcase', component: load('showcase/case1')},
+        {path: 'case2', component: load('showcase/case2')}
+      ]
+    },
+    // Always leave this last one
+    { path: '*', component: load('Error404') } // Not found
+  ]
+})
